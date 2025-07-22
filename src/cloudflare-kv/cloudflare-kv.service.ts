08f7cf35a708e2c4e93d3dde82627de2b29588a7
digitalnomad91 <andrew.c.corbin@gmail.com>
2025-07-21 22:33:28 -0500

    Formatting.

--- src/cloudflare-kv/cloudflare-kv.service.ts
+++ src/cloudflare-kv/cloudflare-kv.service.ts
@@ -5,66 +5,54 @@ import { KvNamespaces } from './models/kv-namespaces.enum';
 
 @Injectable()
 export class CloudflareKvService implements OnModuleInit {
-  private edgeKvUrl: string;
-  private authorizationToken: string;
+    private edgeKvUrl: string;
+    private authorizationToken: string;
 
-  constructor(
-    private readonly configService: ConfigService,
-    private readonly logService: LogService,
-  ) {}
+    constructor(
+        private readonly configService: ConfigService,
+        private readonly logService: LogService
+    ) {}
 
-  onModuleInit() {
-    this.edgeKvUrl = this.configService.get('EDGE_KV_URL');
-    this.authorizationToken = this.configService.get(
-      'EDGE_KV_AUTHORIZATION_TOKEN',
-    );
-  }
-
-  async createKeyValueForNamespace(
-    namespace: KvNamespaces,
-    key: string,
-    value?: string | object,
-  ) {
-    try {
-      const requestOptions = {
-        method: 'POST',
-        headers: {
-          authorizationToken: this.authorizationToken,
-        },
-      };
-
-      const response = await fetch(
-        `${this.edgeKvUrl}/${namespace}/${key}`,
-        requestOptions,
-      );
-
-      return response;
-    } catch (e) {
-      this.logService.error('Failed to update workers KV', {
-        errorMessage: e.message,
-      });
+    onModuleInit() {
+        this.edgeKvUrl = this.configService.get('EDGE_KV_URL');
+        this.authorizationToken = this.configService.get('EDGE_KV_AUTHORIZATION_TOKEN');
     }
-  }
 
-  async deleteKeyForNamespace(namespace: KvNamespaces, key: string) {
-    try {
-      const requestOptions = {
-        method: 'DELETE',
-        headers: {
-          authorizationToken: this.authorizationToken,
-        },
-      };
+    async createKeyValueForNamespace(namespace: KvNamespaces, key: string, value?: string | object) {
+        try {
+            const requestOptions = {
+                method: 'POST',
+                headers: {
+                    authorizationToken: this.authorizationToken,
+                },
+            };
 
-      const response = await fetch(
-        `${this.edgeKvUrl}/${namespace}/${key}`,
-        requestOptions,
-      );
+            const response = await fetch(`${this.edgeKvUrl}/${namespace}/${key}`, requestOptions);
 
-      return response.json();
-    } catch (e) {
-      this.logService.error('Failed to update workers KV', {
-        errorMessage: e.message,
-      });
+            return response;
+        } catch (e) {
+            this.logService.error('Failed to update workers KV', {
+                errorMessage: e.message,
+            });
+        }
     }
-  }
+
+    async deleteKeyForNamespace(namespace: KvNamespaces, key: string) {
+        try {
+            const requestOptions = {
+                method: 'DELETE',
+                headers: {
+                    authorizationToken: this.authorizationToken,
+                },
+            };
+
+            const response = await fetch(`${this.edgeKvUrl}/${namespace}/${key}`, requestOptions);
+
+            return response.json();
+        } catch (e) {
+            this.logService.error('Failed to update workers KV', {
+                errorMessage: e.message,
+            });
+        }
+    }
 }
