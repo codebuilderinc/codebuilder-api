9075c5ece721b033218faac94aa9a9a9def3ab8a
digitalnomad91 <andrew.c.corbin@gmail.com>
2025-08-05 16:30:55 -0500

    Moved metadata model to job model file to fix circular dependency error.

--- src/jobs/models/job.model.ts
+++ src/jobs/models/job.model.ts
@@ -2,7 +2,6 @@ import { Field, ObjectType, Int } from '@nestjs/graphql';
 import { BaseModel } from '../../common/models/base.model';
 import { Company } from './company.model';
 import { JobTag } from './job-tag.model';
-import { JobMetadata } from './job-metadata.model';
 import { JobSource } from './job-source.model';
 
 /**
@@ -85,7 +84,7 @@ export class Job extends BaseModel {
      * Metadata associated with this job
      * One-to-many relationship with JobMetadata
      */
-    @Field(() => [JobMetadata], { nullable: true })
+    @Field(() => [() => JobMetadata], { nullable: true })
     metadata?: JobMetadata[];
 
     /**
@@ -94,4 +93,39 @@ export class Job extends BaseModel {
      */
     @Field(() => [JobSource], { nullable: true })
     sources?: JobSource[];
+}
+
+/**
+ * GraphQL ObjectType for JobMetadata entities
+ * Represents arbitrary metadata associated with jobs
+ */
+@ObjectType()
+export class JobMetadata extends BaseModel {
+    /**
+     * The ID of the job this metadata belongs to
+     * @example 1
+     */
+    @Field(() => Int)
+    jobId: number;
+
+    /**
+     * The name/key of the metadata
+     * @example "salary_range"
+     */
+    @Field()
+    name: string;
+
+    /**
+     * The value of the metadata
+     * @example "$80,000 - $120,000"
+     */
+    @Field()
+    value: string;
+
+    /**
+     * The job this metadata belongs to
+     * Many-to-one relationship with Job
+     */
+    @Field(() => () => typeof Job)
+    job: Job;
 }
