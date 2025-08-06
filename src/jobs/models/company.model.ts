7da92d6afb986845062193dd700c604e7e755228
digitalnomad91 <andrew.c.corbin@gmail.com>
2025-08-06 17:39:57 -0500

    Removed graphql job models for now.

--- src/jobs/models/company.model.ts
+++ src/jobs/models/company.model.ts
@@ -1,24 +1,24 @@
-import { Field, ObjectType, Int } from '@nestjs/graphql';
-import { BaseModel } from '../../common/models/base.model';
-import { Job } from './job.model';
+// import { Field, ObjectType, Int } from '@nestjs/graphql';
+// import { BaseModel } from '../../common/models/base.model';
+// import { Job } from './job.model';
 
-/**
- * GraphQL ObjectType for Company entities
- * Represents a company that posts jobs
- */
-@ObjectType()
-export class Company extends BaseModel {
-    /**
-     * The name of the company
-     * @example "Tech Corp Inc."
-     */
-    @Field()
-    name: string;
+// /**
+//  * GraphQL ObjectType for Company entities
+//  * Represents a company that posts jobs
+//  */
+// @ObjectType()
+// export class Company extends BaseModel {
+//     /**
+//      * The name of the company
+//      * @example "Tech Corp Inc."
+//      */
+//     @Field()
+//     name: string;
 
-    /**
-     * Jobs posted by this company
-     * One-to-many relationship with Job
-     */
-    @Field(() => [Job], { nullable: true })
-    jobs?: Job[];
-}
+//     /**
+//      * Jobs posted by this company
+//      * One-to-many relationship with Job
+//      */
+//     @Field(() => [Job], { nullable: true })
+//     jobs?: Job[];
+// }
