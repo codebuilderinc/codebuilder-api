b1e5e1b76bd31635625476f2d6a863e96538b300
digitalnomad91 <andrew.c.corbin@gmail.com>
2025-09-08 04:05:10 -0500

    Moved auth module to project root folder.

--- src/common/auth/models/error-messages.ts
+++
@@ -1,5 +0,0 @@
-export const AUTH_ERROR_MESSAGES = {
-  MISSING_TOKEN: 'No token provided',
-  WALLET_ALREADY_IN_USE: 'This wallet is already registered to another user',
-  INVALID_SIGNATURE: 'Invalid signature',
-};
--- src/common/auth/models/jwt-payload.model.ts
+++
@@ -1,9 +0,0 @@
-export interface JwtPayload extends JwtUserInfo {
-  accessToken: string;
-  refreshToken: string;
-}
-
-export interface JwtUserInfo {
-  userId: number;
-  username: string;
-}
