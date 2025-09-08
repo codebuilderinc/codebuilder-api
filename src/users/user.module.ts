4a80f09d03794fe9f5599641f048d92f7dd7c186
digitalnomad91 <andrew.c.corbin@gmail.com>
2025-09-08 04:06:13 -0500

    Updated auth import to new path.

--- src/users/user.module.ts
+++ src/users/user.module.ts
@@ -1,9 +1,9 @@
 import { Module } from '@nestjs/common';
 import { UserResolver } from './user.resolver';
 import { UserService } from './user.service';
-import { PasswordService } from '../common/auth/password.service';
+import { PasswordService } from '../auth/password.service';
 import { UserController } from './user.controller';
-import { AuthModule } from '../common/auth/auth.module';
+import { AuthModule } from '../auth/auth.module';
 import { ConfigService } from './../common/configs/config.service';
 import { RedisModule } from '../common/redis/redis.module';
 
