7fac57b0b2265b8c55c273fddd3cf063932d5cba
digitalnomad91 <andrew.c.corbin@gmail.com>
2025-10-04 02:15:02 -0500

    Updated prisma.

--- src/generated/nestjs-dto/job.entity.ts
+++ src/generated/nestjs-dto/job.entity.ts
@@ -1,24 +1,26 @@
-import { Prisma } from '@prisma/client';
-import { Company } from './company.entity';
-import { JobTag } from './jobTag.entity';
-import { JobMetadata } from './jobMetadata.entity';
+
+import {Prisma} from '@prisma/client'
+import {Company} from './company.entity'
+import {JobTag} from './jobTag.entity'
+import {JobMetadata} from './jobMetadata.entity'
+
 
 export class Job {
-  id: number;
-  title: string;
-  companyId: number | null;
-  company?: Company | null;
-  author: string | null;
-  location: string | null;
-  url: string;
-  postedAt: Date | null;
-  description: string | null;
-  isRemote: boolean | null;
-  createdAt: Date;
-  updatedAt: Date;
-  source: string | null;
-  externalId: string | null;
-  data: Prisma.JsonValue | null;
-  tags?: JobTag[];
-  metadata?: JobMetadata[];
+  id: number ;
+title: string ;
+companyId: number  | null;
+company?: Company  | null;
+author: string  | null;
+location: string  | null;
+url: string ;
+postedAt: Date  | null;
+description: string  | null;
+isRemote: boolean  | null;
+createdAt: Date ;
+updatedAt: Date ;
+source: string  | null;
+externalId: string  | null;
+data: Prisma.JsonValue  | null;
+tags?: JobTag[] ;
+metadata?: JobMetadata[] ;
 }
