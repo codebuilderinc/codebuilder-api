fb5b93ed32052a032d4f915cc08947b12cfbfb9d
digitalnomad91 <andrew.c.corbin@gmail.com>
2025-10-09 17:05:44 -0500

    Added sentry; Removed otel; Updated logger;

--- src/app.module.ts
+++ src/app.module.ts
@@ -1,9 +1,9 @@
-import { GraphQLModule } from '@nestjs/graphql';
+//import { GraphQLModule } from '@nestjs/graphql';
 import { Logger, MiddlewareConsumer, Module, RequestMethod } from '@nestjs/common';
 import { ConfigModule } from './common/configs/config.module';
 import { PrismaModule, loggingMiddleware } from 'nestjs-prisma';
 import { AppService } from './app.service';
-import config from './common/configs/config';
+//import config from './common/configs/config';
 import { APP_GUARD } from '@nestjs/core';
 import { AppRouterModule } from './app-router.module';
 import { CloudflareKvModule } from './cloudflare-kv/cloudflare-kv.module';
@@ -12,16 +12,18 @@ import { DatabaseModule } from './common/database/database.module';
 import { QueueModule } from './common/queue/queue.module';
 import { WssModule } from './wss/wss.module';
 import { EventsModule } from './events/events.module';
-import { OpenTelemetryModule } from 'nestjs-otel';
-import { LoggerModule } from './logger/logger.module';
+//import { LoggerModule } from './logger/logger.module';
 import { RedisModule } from './common/redis/redis.module';
 import { NotificationsModule } from './notifications/notifications.module';
 import { LocationModule } from './location/location.module';
 import { ErrorsModule } from './errors/errors.module';
+import { SentryModule } from '@sentry/nestjs/setup';
 
+//import { OpenTelemetryModule } from 'nestjs-otel';
 //import { ConfigModule } from '@nestjs/config';
-//import { AppResolver } from './app.resolver';
 // import { UserModule } from './users/user.module';
+
+//import { AppResolver } from './app.resolver';
 //import { ApolloDriver, ApolloDriverConfig } from '@nestjs/apollo';
 //import { GqlConfigService } from './gql-config.service';
 // import { AllowedBlockchainsGuard } from './blockchains/allowed-blockchains.guard';
@@ -30,24 +32,25 @@ import { ErrorsModule } from './errors/errors.module';
 //import { openTelemetryConfig } from './open-telemetry.config';
 // import { TradeModule } from './trades/trade.module';
 
-const OpenTelemetryModuleConfig = OpenTelemetryModule.forRoot({
-  metrics: {
-    hostMetrics: true,
-    apiMetrics: {
-      enable: true,
-    },
-  },
-});
+// const OpenTelemetryModuleConfig = OpenTelemetryModule.forRoot({
+//   metrics: {
+//     hostMetrics: true,
+//     apiMetrics: {
+//       enable: true,
+//     },
+//   },
+// });
 
 @Module({
   imports: [
+    SentryModule.forRoot(),
     // Setup NestJS open telemetry auto instrumentation. This requires the configuration
     // to be passed in again for some features (e.g. metrics) to work correctly.
     //OpenTelemetryModule.forRoot(openTelemetryConfig),
     ConfigModule,
     //ConfigModule.forRoot({ isGlobal: true, load: [config] }),
-    LoggerModule,
-    OpenTelemetryModuleConfig,
+    //LoggerModule,
+    //OpenTelemetryModuleConfig,
     RedisModule.forRoot(),
     PrismaModule.forRoot({
       isGlobal: true,
