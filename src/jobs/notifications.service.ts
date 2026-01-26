cfbb196e5b62f394c8b0622a5e15717050859d43
digitalnomad91 <andrew.c.corbin@gmail.com>
2026-01-25 19:07:11 -0600

    Deleted old notification service file.

--- src/jobs/notifications.service.ts
+++
@@ -1,8 +0,0 @@
-// DEPRECATED: Use NotificationsService from 'src/notifications/notifications.service'
-// This file remains temporarily to avoid breaking existing imports. Update imports to:
-// import { NotificationsService } from '../notifications/notifications.service';
-// Then delete this file.
-
-export * from '../notifications/notifications.service';
-export * from '../notifications/interfaces/notification-payload.interface';
-export * from '../notifications/interfaces/subscription-record.interface';
